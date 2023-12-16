3a750929e1651ba335ef069d66b95c70ff6520e8
GauravKaklotar <gaurav.kaklotar03@gmail.com>
2023-12-16 23:10:51 +0530

    Products list completed

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../interface/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  providers: [ProductService]
 })
 export class ProductsComponent {
 
+  allProducts!: IProduct[];
+  product!: IProduct;
+
+  constructor(public productService: ProductService){
+    this.productService.getAllProducts().then((productList: IProduct[]) => {
+      this.allProducts = productList;
+      console.log(this.allProducts);
+    });
+  }
 }
