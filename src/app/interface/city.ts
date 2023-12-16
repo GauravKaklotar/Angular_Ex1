32dc2cbb9003b502f3e66f24dfddbca4ee904430
GauravKaklotar <gaurav.kaklotar03@gmail.com>
2023-12-16 21:28:31 +0530

    json data and interface

---
+++ src/app/interface/city.ts
@@ -0,0 +1,9 @@
+export interface City {
+    id: number;
+    name: string;
+    country: string;
+    population: number;
+    area_sq_km: number;
+    latitude: number;
+    longitude: number;
+}
---
+++ src/app/interface/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    age: number;
+    address: string;
+    phone: string;
+}
