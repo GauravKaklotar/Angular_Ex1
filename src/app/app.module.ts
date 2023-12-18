5eb42cd66b364e8ab550e678354ed9cfa8cb4459
Jarvi64 <129019096+Jarvi64@users.noreply.github.com>
2023-12-18 23:05:29 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UserDetailsComponent } from './users/user-details/user-details.componen
 import { CityDetailsComponent } from './cities/city-details/city-details.component';
 import { FormsModule } from '@angular/forms';
 import { TooltipDirective } from './directives/tooltip.directive';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +27,8 @@ import { TooltipDirective } from './directives/tooltip.directive';
     ProductDetailsComponent,
     UserDetailsComponent,
     CityDetailsComponent,
-    TooltipDirective
+    TooltipDirective,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
